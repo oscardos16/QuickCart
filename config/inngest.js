332d92a47cd40aea8b94965c375f1adecc00e5c5
oscardos16 <oscardosmilitante@gmail.com>
2025-08-01 12:40:30 +0800

    Save

--- config/inngest.js
+++ config/inngest.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
+import Order from "@/models/Order";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
@@ -72,7 +73,7 @@ export const createUserOrder = inngest.createFunction(
                 userId: event.data.userId,
                 items: event.data.items,
                 amount: event.data.amount,
-                address: event.data.address,
+                address: event.data.address, 
                 date: event.data.date
             }
         })
--- models/Order.js
+++ models/Order.js
@@ -2,15 +2,15 @@ import mongoose from "mongoose";
 
 
 const orderSchema = new mongoose.Schema({
-    userId: {type: String, required:true, ref: 'user'},
+    userId: {type: String, required: true, ref: 'user'},
     items: [{
-        product: {type: String, required:true, ref:'product'},
-        quantity: {type: Number, required: true, }
+        product: {type: String, required: true, ref:'product'},
+        quantity: {type: Number, required: true }
     }],
-    amount: { type: Number, required:true },
+    amount: { type: Number, required: true },
     address: { type: String, ref: 'address', required: true },
     status: { type: String, required: true, default: 'Order Placed' },
-    date: { type: Number, required:true },
+    date: { type: Number, required: true }
 
 })
 
